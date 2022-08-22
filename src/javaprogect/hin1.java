826778f3c786b73780d86e9bcdadcb628d00dc5e
Finn183747 <usmanovakbar47@gmail.com>
2022-08-22 18:40:32 +0500

    мой четвертый комид

---
+++ src/javaprogect/hin1.java
@@ -0,0 +1,12 @@
+package javaprogect;
+
+
+public class hin1  {
+    public static void main(String[] args) {
+        int a = 3;
+        while(a <= 30){
+            System.out.print(a + " ");
+            a = a + 3;
+        }
+    }
+}
