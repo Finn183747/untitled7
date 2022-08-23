e79e5697adbfbd4c7eb6a5520138c1cddebff52d
Finn183747 <usmanovakbar47@gmail.com>
2022-08-23 18:07:11 +0500

    мой пятый комид

---
+++ src/javaprogect/Hin02.java
@@ -0,0 +1,11 @@
+package javaprogect;
+
+public class Hin02 {
+    public static void main(String[] args) {
+      int a = 1;
+      while(a <=2048) {
+          System.out.print(a + " ");
+          a *=2;
+      }
+    }
+}
